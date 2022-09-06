cd1e6f6a985859420cd63f2d0c060888067d194f
ej18045 <111259823+ej18045@users.noreply.github.com>
2022-09-07 09:33:39 +1200

    Add files via upload

---
+++ 01_help_GUI.py
@@ -0,0 +1,13 @@
+from tkinter import ^
+from functools import partial
+
+import random
+
+
+class Converter:
+    def __init__(self):
+        background_color = "light blue"
+
+        self.converter_frame = Frame(width=600, height=600, bg=background_color)
+        self.converter_frame.grid()
+
