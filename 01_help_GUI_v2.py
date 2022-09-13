ec5b198394d8f988f1ed73be291cc9c965ffb34d
ej18045 <111259823+ej18045@users.noreply.github.com>
2022-09-14 09:09:15 +1200

    Add files via upload

--- 01_help_GUI_v2.py
+++ 01_help_GUI_v2.py
@@ -1,14 +1,14 @@
-from tkinter import ^
+from tkinter import *
 from functools import partial  # to make sure there aren't any unnecessary windows
 
 import random
 
 
 class Converter:
-    def __init__(self):
+    def __init__(self, parent):
 
         # variable formatting
-        background_color = "light blue"
+        background_color = "royal blue"
 
         # Converter Main Screen GUI
         self.converter_frame = Frame(width=640, height=480, bg=background_color,
@@ -25,7 +25,7 @@ class Converter:
         # Help Button (row 1)
         self.help_button = Button(self.converter_frame, text="Help",
                                   font=("Ubuntu", "14"),
-                                  padx=10, pady=10)
+                                  padx=10, pady=10, command=self.help)
         self.help_button.grid(row=1)
 
     def help(self):
@@ -48,7 +48,8 @@ class Help:
         self.help_box.protocol('WM_DELETE_WINDOW', partial(self.close_help, partner))
 
         # Set up GUI Frame
-        self.help_frame = Frame(self.help_box, width=300, bg=background)
+        self.help_frame = Frame(self.help_box, width=300, bg=background,
+                                pady=10)
         self.help_frame.grid()
 
         # Set up Help heading (row 0)
@@ -57,46 +58,24 @@ class Help:
         self.how_heading.grid(row=0)
 
         # Help text (label, row 1)
-        self.help_text = Label(self.help_frame, text="",
+        self.help_text = Label(self.help_frame, text="Intructions",
                                justify=LEFT, width=40, bg=background, wrap=250)
         self.help_text.grid(row=1)
 
         # Dismiss button (row 2)
         self.dismiss_btn = Button(self.help_frame, text="Dismiss",
-                                  width=10, bg="orange", font="ubuntu 10 bold",
-                             command=partial(self.close_help, partner))
+                                  width=10, bg="ghost white", font="ubuntu 10 bold",
+                                  command=partial(self.close_help, partner))
         self.dismiss_btn.grid(row=2, pady=10)
 
     def close_help(self, partner):
         # Put help button back to normal
-    partner.help_button.config(state=NORMAL)
-    self.help_box.destroy()
+        partner.help_button.config(state=NORMAL)
+        self.help_box.destroy()
 
 # main routine
 if __name__ == "__main__":
     root = Tk()
     root.title("Temperature Converter")
-    something = Converter()
+    something = Converter(root)
     root.mainloop()
-
-class Help:
-    def __init__(self):
-
-        self.help_box = Toplevel()
-
-        # If user presses 'x', make main dialogue re-appear.
-        self.help_box.protocol('WM_DELETE_WINDOW', self.close_help)
-
-        self.help_frame = Frame(self.help_box)
-        self.help_frame,grid()
-
-        # Help Heading
-        self.help_heading_label = Label(self.help_frame, text="Help",
-                                        font=("Ubuntu", "16", "bold"))
-        self.help_heading_label.grid(row=0)
-
-        # Help Text
-        self.help_text = Label(self.help_frame, text="")
-        self.help_text.grid(row=1)
-
-        # Dismiss Button
